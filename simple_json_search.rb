209db76128b841c07b762b26417f881f2e5165dd
Orest Kostiuk <xxmolekula@gmail.com>
2020-08-07 15:10:03 +0300

    refactored

--- simple_json_search.rb
+++ simple_json_search.rb
@@ -1,7 +1,8 @@
 require 'pry'
 require 'json'
 class SimpleJsonSearch
-	JSON_DATA = JSON.load(File.open('./data.json')).freeze
+
+	attr_reader :result
 
 	def initialize(query)
 		@query = query
@@ -9,7 +10,7 @@ class SimpleJsonSearch
 
 	def call
 		get_query
-		check_query_in_quotes
+		get_query_in_quotes
 		search
 		@result
 	end
@@ -17,22 +18,32 @@ class SimpleJsonSearch
 	private
 
 	def search
-		@result = JSON_DATA.select { |item| item.values.join(',').match(@first_query)}
-		@result = @result.select { |item| item.values.join(',').match(@second_query)} if @second_query
-		if @result.empty?
-			@result = JSON_DATA
-			@first_query.split(' ').each do |query|
-				@result = @result.select { |item| item.values.join(',').match(query)}
-			end
+		query('json_data', @search_value)
+		query('result', @query_in_quotes) if @query_in_quotes
+		search_by_different_keys if @result.empty?
+	end
+
+	def search_by_different_keys
+		@result = json_data
+		@search_value.split(' ').each do |query|
+			query('result', query)
 		end
 	end
 
-	def check_query_in_quotes
-		@second_query = @query.scan(/[^"|']+/).last.delete_suffix(' ') if @query.scan(/[^"|']+/).count > 1
+	def get_query_in_quotes
+		@query_in_quotes = @query.scan(/[^"|']+/)[1].delete_suffix(' ') if @query.scan(/[^"|']+/).count > 1
 	end
 
 	def get_query
-		@first_query = @query.scan(/[^"|']+/).first.delete_suffix(' ')
+		@search_value = @query.scan(/[^"|']+/).first.delete_suffix(' ')
+	end
+
+	def query(data, query)
+		@result = __send__(data).select { |item| item.values.join(',').match(query)}
+	end
+
+	def json_data
+		JSON.load(File.open('./data.json'))
 	end
 
 end
